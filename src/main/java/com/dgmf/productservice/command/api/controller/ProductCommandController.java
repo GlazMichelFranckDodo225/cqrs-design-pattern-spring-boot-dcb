b859a8be6659f6ae664cdc15588c61f92bebe6dc
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-25 18:01:10 +0200

    Creation of the "String addProduct(@RequestBody ProductRestModel productRestModel)" method in PostMapping and creation of the "ProductRestModel" class

--- src/main/java/com/dgmf/productservice/command/api/controller/ProductCommandController.java
+++ src/main/java/com/dgmf/productservice/command/api/controller/ProductCommandController.java
@@ -1,9 +1,16 @@
 package com.dgmf.productservice.command.api.controller;
 
+import com.dgmf.productservice.command.api.model.ProductRestModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductCommandController {
+    @PostMapping
+    public String addProduct(@RequestBody ProductRestModel productRestModel) {
+        return "Product Added";
+    }
 }
