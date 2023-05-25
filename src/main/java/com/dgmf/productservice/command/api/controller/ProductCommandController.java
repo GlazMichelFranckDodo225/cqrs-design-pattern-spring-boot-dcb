060f6b4b0bfe386584565d171f6dad4b17e59861
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-25 18:20:11 +0200

    Creation of the "CreateProductCommand" class and finalization of the "String addProduct(@RequestBody ProductRestModel productRestModel)" method in the "ProductCommandController"

--- src/main/java/com/dgmf/productservice/command/api/controller/ProductCommandController.java
+++ src/main/java/com/dgmf/productservice/command/api/controller/ProductCommandController.java
@@ -1,11 +1,14 @@
 package com.dgmf.productservice.command.api.controller;
 
+import com.dgmf.productservice.command.api.commands.CreateProductCommand;
 import com.dgmf.productservice.command.api.model.ProductRestModel;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -18,6 +21,16 @@ public class ProductCommandController {
 
     @PostMapping
     public String addProduct(@RequestBody ProductRestModel productRestModel) {
+        CreateProductCommand createProductCommand =
+                CreateProductCommand.builder()
+                        .productId(UUID.randomUUID().toString())
+                        .name(productRestModel.getName())
+                        .price(productRestModel.getPrice())
+                        .quantity(productRestModel.getQuantity())
+                        .build();
+
+        String result = commandGateway.sendAndWait(createProductCommand);
+        
         return "Product Added";
     }
 }
