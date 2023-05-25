f20ee499dfe80fc4a4d42ca23496938704fd79cf
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-25 18:05:37 +0200

    In the "Product Command Controller" class, injection by constructor of a "commandGateway" object

--- src/main/java/com/dgmf/productservice/command/api/controller/ProductCommandController.java
+++ src/main/java/com/dgmf/productservice/command/api/controller/ProductCommandController.java
@@ -1,6 +1,7 @@
 package com.dgmf.productservice.command.api.controller;
 
 import com.dgmf.productservice.command.api.model.ProductRestModel;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/products")
 public class ProductCommandController {
+    private CommandGateway commandGateway;
+
+    public ProductCommandController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
     @PostMapping
     public String addProduct(@RequestBody ProductRestModel productRestModel) {
         return "Product Added";
