4a64cc0e6ffd66f422a4289db04f30a7dbcde362
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-25 17:48:55 +0200

    Creating the ProductCommandController

---
+++ src/main/java/com/dgmf/productservice/command/api/controller/ProductCommandController.java
@@ -0,0 +1,9 @@
+package com.dgmf.productservice.command.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductCommandController {
+}
