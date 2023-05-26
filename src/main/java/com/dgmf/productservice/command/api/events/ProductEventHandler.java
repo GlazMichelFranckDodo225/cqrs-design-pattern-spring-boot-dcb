f702012f666ef9704cd6df1836f886bdb157d551
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-26 10:44:45 +0200

    Creation of the "void on(ProductCreatedEvent productCreatedEvent)" method of the ProductEventHandler class

--- src/main/java/com/dgmf/productservice/command/api/events/ProductEventHandler.java
+++ src/main/java/com/dgmf/productservice/command/api/events/ProductEventHandler.java
@@ -1,8 +1,12 @@
 package com.dgmf.productservice.command.api.events;
 
+import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductEventHandler {
-    
+    @EventHandler
+    public void on(ProductCreatedEvent productCreatedEvent) {
+
+    }
 }
