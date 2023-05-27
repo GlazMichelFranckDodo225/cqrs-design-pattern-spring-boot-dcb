e703157b881cc977edeb66900322d83e858e95c5
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-27 04:42:02 +0200

    In the "List<ProductRestModel> getAllProducts()" method, using QueryGateway to query "getProductsQuery"

--- src/main/java/com/dgmf/productservice/query/api/controller/ProductQueryController.java
+++ src/main/java/com/dgmf/productservice/query/api/controller/ProductQueryController.java
@@ -1,6 +1,9 @@
 package com.dgmf.productservice.query.api.controller;
 
 import com.dgmf.productservice.command.api.model.ProductRestModel;
+import com.dgmf.productservice.query.api.queries.GetProductsQuery;
+import org.axonframework.messaging.responsetypes.ResponseType;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,14 @@ public class ProductQueryController {
 
     @GetMapping
     public List<ProductRestModel> getAllProducts() {
-        return null;
+        GetProductsQuery getProductsQuery = new GetProductsQuery();
+
+        // Using QueryGateway to query "getProductsQuery"
+        List<ProductRestModel> productRestModels = queryGateway.query(getProductsQuery,
+                // Response type back (list of "ProductRestModel")
+                ResponseTypes.multipleInstancesOf(ProductRestModel.class))
+                .join();
+
+        return productRestModels;
     }
 }
