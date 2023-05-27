e0fe758d9c2e97c1c70279aee8f89fd8a4cc29db
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-27 05:17:14 +0200

    With the "List<ProductRestModel> handle(GetProductsQuery getProductsQuery)" handling of the "getProductsQuery" from the QueryGateway after retrieving of all the Products from the DB

--- src/main/java/com/dgmf/productservice/query/api/projection/ProductProjection.java
+++ src/main/java/com/dgmf/productservice/query/api/projection/ProductProjection.java
@@ -1,7 +1,14 @@
 package com.dgmf.productservice.query.api.projection;
 
+import com.dgmf.productservice.command.api.data.Product;
+import com.dgmf.productservice.command.api.model.ProductRestModel;
 import com.dgmf.productservice.command.api.repository.ProductRepository;
+import com.dgmf.productservice.query.api.queries.GetProductsQuery;
+import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 // To handle "getProductsQuery" from the QueryGateway
 @Component
@@ -11,5 +18,23 @@ public class ProductProjection {
 
     public ProductProjection(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    // Handling of the "getProductsQuery" from the QueryGateway
+    @QueryHandler
+    public List<ProductRestModel> handle(GetProductsQuery getProductsQuery) {
+        // Retrieving of all the Products
+        List<Product> products = productRepository.findAll();
+
+        // Mapping "products" to "productRestModels"
+        List<ProductRestModel> productRestModels = products.stream()
+                .map(product -> ProductRestModel.builder()
+                        .name(product.getName())
+                        .price(product.getPrice())
+                        .quantity(product.getQuantity())
+                        .build())
+                .collect(Collectors.toList());
+
+        return productRestModels;
     }
 }
