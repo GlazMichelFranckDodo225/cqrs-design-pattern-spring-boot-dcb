011bbf969595de5a5f63fcf1a9d2d1a45417a600
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-27 04:55:45 +0200

    In the "ProductProjection" class, injection by constructor of a "productRepository" to retrieve the Products stored in the DB

--- src/main/java/com/dgmf/productservice/query/api/projection/ProductProjection.java
+++ src/main/java/com/dgmf/productservice/query/api/projection/ProductProjection.java
@@ -1,8 +1,15 @@
 package com.dgmf.productservice.query.api.projection;
 
+import com.dgmf.productservice.command.api.repository.ProductRepository;
 import org.springframework.stereotype.Component;
 
 // To handle "getProductsQuery" from the QueryGateway
 @Component
 public class ProductProjection {
+    // To get Data back from the DB
+    private ProductRepository productRepository;
+
+    public ProductProjection(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
