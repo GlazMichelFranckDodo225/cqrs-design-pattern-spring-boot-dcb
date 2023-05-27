d131011d71371baede565bb8edbd9fb8849da71b
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-27 04:50:06 +0200

    Creating the "ProductProjection" class to handle "getProductsQuery" from the QueryGateway

---
+++ src/main/java/com/dgmf/productservice/query/api/projection/ProductProjection.java
@@ -0,0 +1,8 @@
+package com.dgmf.productservice.query.api.projection;
+
+import org.springframework.stereotype.Component;
+
+// To handle "getProductsQuery" from the QueryGateway
+@Component
+public class ProductProjection {
+}
