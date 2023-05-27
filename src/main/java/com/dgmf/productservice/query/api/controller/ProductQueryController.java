50ab6e8467b325b8d14d8cd0c5097f104a2fbf4a
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-27 04:06:20 +0200

    In the "ProductQueryController" controller, initial implementation of the "List<ProductRestModel> getAllProducts()" method in "@GetMapping"

--- src/main/java/com/dgmf/productservice/query/api/controller/ProductQueryController.java
+++ src/main/java/com/dgmf/productservice/query/api/controller/ProductQueryController.java
@@ -1,10 +1,17 @@
 package com.dgmf.productservice.query.api.controller;
 
+import com.dgmf.productservice.command.api.model.ProductRestModel;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductQueryController {
-
+    @GetMapping
+    public List<ProductRestModel> getAllProducts() {
+        return null;
+    }
 }
